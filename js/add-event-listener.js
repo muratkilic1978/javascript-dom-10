bf8d184717e0ad2cf1fe3976a84e8342d9366d0f
muratkilic1978 <muratkilic1978@gmail.com>
2020-01-04 16:51:32 +0100

    add-event-listener.js was updated

--- js/add-event-listener.js
+++ js/add-event-listener.js
@@ -4,20 +4,23 @@ const listElem = document.querySelectorAll(".hot");
 for (let i = 0; i < listElem.length; i++) {
     console.log(listElem[i].textContent);
     //listElem[i].style.backgroundColor = "yellow";
-    listElem[i].addEventListener("click",makeRed);
-}
-
-function makeRed(){
-    console.log(this);
-    let tempList = this.classList.toggle("shot");
-    console.log(tempList);
     
+    // Adding EventListener calling an External function makeRed()
+    //listElem[i].addEventListener("click",makeRed);
+    
+    // Adding Eventlistener with an anonymous function
+    listElem[i].addEventListener("click", function(){
+        console.log(this);
+        let tempList = this.classList.toggle("shot");
+        console.log(tempList); 
+    });
 }
 
-
-
-//listItem.addEventListener("click",changeColor);
-//
-//function changeColor(){
-//    console.log("liste Item was clicked!!!");
+//External function with the name makeRed() 
+//function makeRed(){
+//    console.log(this);
+//    let tempList = this.classList.toggle("shot");
+//    console.log(tempList);
+//    
 //}
+
